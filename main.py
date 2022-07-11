a7eedfa9cd8e2c45ae3da1d84aa4d91626013c8e
Bien Ebonia <bebonia@gmail.com>
2022-07-11 21:44:45 +1000

    gradient descent v2

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
+from multiprocessing.sharedctypes import Value
 import numpy as np
 
-def gradient_descent(gradient,x,y, start, learn_rate, n_iter=50,tolerance=1e-6):
-    vector = start
+def gradient_descent(gradient,x,y, start, learn_rate, n_iter=50,tolerance=1e-6,
+dtype='float64'):
+
+    # Check if gradient is callable
+    if not callable(gradient):
+        raise TypeError("'gradient' must be callable")
+
+    # Setting up the data type for NumPy Arrays
+    dtype_ = np.dtype(dtype)
+
+    # Converting x and y to NumPy Arrays
+    x, y = np.array(x, dtype=dtype_), np.array(y,dtype=dtype_)
+    if x.shape[0] != y.shape[0]:
+        raise ValueError("'x' and 'y' lengths do not match")
+    
+    # Initializing the values of the  variables
+    vector = np.array(start,dtype=dtype_)
+ 
+    # Setting up and checking the learning rate
+    learn_rate = np.array(learn_rate, dtype=dtype_)
+    if np.any(learn_rate <= 0):
+        raise ValueError("'learn_rate' must be greater than zero")
+
+    # Setting up and checking the maximal number of iterations
+    n_iter = int(n_iter)
+    if n_iter <= 0:
+        raise ValueError("'n_iter' must be greater than zero")
+
+    # Setting up and checking the tolerance
+    tolerance = np.array(tolerance, dtype=dtype_)
+    if np.any(tolerance <= 0):
+        raise ValueError("'tolerance' must be greater than zero")
+
+    # Performing the gradient descent loop
     for _ in range(n_iter):
-        diff = -learn_rate * np.array(gradient(x,y,vector))
+        # Recalculating the difference
+        diff = -learn_rate * np.array(gradient(x, y, vector), dtype_)
+
+        # Checking if the absolute difference is small enough
         if np.all(np.abs(diff) <= tolerance):
             break
+
+        # Updating the values of the variables
         vector += diff
-    
-    return vector
+
+    return vector if vector.shape else vector.item()
 
 def ssr_gradient(x,y,b):
     res = b[0] + b[1] * x - y
